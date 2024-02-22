beba1389f77ca1d8677cc503f71fd1bede5b6f5c
SusanneRobledo <susanne.robledo@web.de>
2024-02-22 13:27:33 +0100

    single fruitlist seperate component

--- src/app/fruitlist/singlefruit/singlefruit.component.ts
+++ src/app/fruitlist/singlefruit/singlefruit.component.ts
@@ -8,5 +8,16 @@ import { Component, Input } from '@angular/core';
   styleUrl: './singlefruit.component.scss',
 })
 export class SinglefruitComponent {
-  @Input('testNumber') fruitNumber = 0;
+  @Input('testfruit') fruit = {
+    name: 'Apfel',
+    img: 'apple.png',
+    description:
+      'Äpfel sind aufgrund ihres hohen Wassergehalts kalorienarm und enthalten nur Spuren von Fett und Eiweiß, dafür aber rund zwei Prozent Ballaststoffe und etwa elf Prozent Kohlenhydrate. Äpfel enthalten auch viele Vitamine und Mineralstoffe und sind daher eine wichtige Quelle für uns - zum Beispiel für Vitamin C.',
+    genus: 'Kernobstgewächsen innerhalb der Familie der Rosengewächse',
+    stars: 0.5,
+    reviews: [
+      { name: 'Kevin W.', text: 'ist lecker' },
+      { name: 'Arne P.', text: 'nicht so meins' },
+    ],
+  };
 }
